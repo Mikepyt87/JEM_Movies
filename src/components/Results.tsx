7e9b4feba6d06c0182998d7b040cd9b66534f3e6
Mikepyt87 <81245551+Mikepyt87@users.noreply.github.com>
2022-11-07 11:57:48 -0500

    lunch

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,7 +1,24 @@
+import Movie from "../models/Movie";
 import "./Results.css";
 
-const Results = () => {
-  return <div className="Results">Results works</div>;
+//2
+
+interface Props {
+  oneMovie: Movie;
+}
+
+//3
+const Results = ({ oneMovie }: Props) => {
+  return (
+    <li className="Results">
+      <h3>{oneMovie.title}</h3>
+      <img
+        src={`https://image.tmdb.org/t/p/w500/${oneMovie.poster_path}`}
+        alt="movieImage"
+      />
+      <button className="watchList">Add to your Watchlist</button>
+    </li>
+  );
 };
 
 export default Results;
